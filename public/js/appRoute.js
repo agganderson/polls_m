e7b29d9fa55800603319bcedd8bfa4e85b6130a2
Alexandra Anderson <aliganderson@gmail.com>
2016-05-31 19:19:42 -0700

    working on 'angular not defined error

--- public/js/appRoute.js
+++ public/js/appRoute.js
@@ -1,18 +1,18 @@
-angular.module('pollsApp', []);.config(function($routeProvider){
-		$routeProvider
-			.when('/login', {
-				templateUrl: 'partials/login.html'
-			})
-			.when('/dashboard', {
-				templateUrl: 'partials/dashboard.html'
-			})
-			.when('/create', {
-				templateUrl: 'partials/create.html'
-			})
-			.when('/poll/:id', {
-				templateUrl: 'partials/poll.html'
-			})
-			.otherwise({
-				redirectTo: '/'
-			});
-	});
+angular.module("appRoutes", []).config(function($routeProvider){
+	$routeProvider
+		.when('/login', {
+			templateUrl: 'partials/login.html'
+		})
+		.when('/dashboard', {
+			templateUrl: 'partials/dashboard.html'
+		})
+		.when('/create', {
+			templateUrl: 'partials/create.html'
+		})
+		.when('/poll/:id', {
+			templateUrl: 'partials/poll.html'
+		})
+		.otherwise({
+			redirectTo: '/'
+		});
+});
--- server.js
+++ server.js
@@ -1,14 +1,16 @@
 var express = require('express');
 var app = express();
-var bodyParser = require('body-parser');
 var path = require('path');
+
+var bodyParser = require('body-parser');
 app.use(bodyParser.json());
+
+app.use(express.static(path.join(__dirname, "./public")));
 
 require('./server/config/mongoose.js');
 require('./server/config/routes.js')(app);
 
-app.use(express.static(path.join(__dirname, './client')));
-
 app.listen(8000, function(){
 	console.log('Listening on port 8000');
 });
+
